6e1aeb2dd40cc7f2d50e10873c4aeba21593200c
Rasika Mahabare <rasikamahabare@gmail.com>
2025-11-23 23:15:48 +0530

    Remove db import, use PostgreSQL

--- app/api/links/[code]/route.ts
+++ app/api/links/[code]/route.ts
@@ -1,15 +1,3 @@
-// import { sql } from "@vercel/postgres";
-// import { NextResponse } from "next/server";
-
-// export async function GET(req: Request, { params }: any) {
-//   const code = params.code;
-//   const result = await sql`SELECT * FROM links WHERE code = ${code}`;
-
-//   if (!result?.rowCount) return NextResponse.json({ error: "Not found" }, { status: 404 });
-
-//   return NextResponse.json(result.rows[0]);
-// }
-
 // app/[code]/route.ts
 import { sql } from "@vercel/postgres";
 import { NextResponse } from "next/server";
@@ -20,10 +8,9 @@ export async function GET(req: Request, { params }: { params: { code: string } }
   const result = await sql`SELECT * FROM links WHERE code = ${code}`;
 
   if (!result?.rowCount) {
-    return NextResponse.json({ error: "Not found" }, { status: 404 });
+    return NextResponse.json({ error: "Link not found" }, { status: 404 });
   }
 
   // Redirect to the original URL
   return NextResponse.redirect(result.rows[0].url);
 }
-
